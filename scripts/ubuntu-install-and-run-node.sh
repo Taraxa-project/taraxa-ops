a7925ed089984a35e72d5ac0c0d9ee5a146ee8d8
Henrique Fernandes <henrique@toptal.com>
2019-09-12 19:15:52 -0300

    Initial install script ubuntu
    This is a basic and initial script to run on a clean ubuntu 18.04, the
    script should install docker and run a taraxa-node using docker

---
+++ scripts/ubuntu-install-and-run-node.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# Basic script to prepare a Ubuntu machine to run Taraxa Node using docker.
+# This is initial developed to be used as startup script for cloud virtual machines to help new users to get a node running.
+
+TARAXA_NODE_PATH=/opt/taraxa-node
+TARAXA_NODE_BOOT_NODE_ADDRESS=35.238.156.27
+TARAXA_NODE_CONF_PATH=${TARAXA_NODE_PATH}/conf_taraxa.json
+TARAXA_NODE_DB_PATH=${TARAXA_NODE_PATH}/taraxadb
+TARAXA_NODE_DOCKER_IMAGE=taraxa/taraxa-node:latest
+
+TARAXA_NODE_CONF=$(cat <<EOF
+{
+  "node_secret": "TARAXA_NODE_NODE_SECRET",
+  "db_path": "/taraxadb",
+  "dag_processing_threads": 1,
+  "network_address": "0.0.0.0",
+  "network_listen_port": 10002,
+  "network_simulated_delay": 0,
+  "network_transaction_interval": 0,
+  "network_bandwidth": 160,
+  "network_boot_nodes": [
+    {
+      "id": "7b1fcf0ec1078320117b96e9e9ad9032c06d030cf4024a598347a4623a14a421d4f030cf25ef368ab394a45e920e14b57a259a09c41767dd50d1da27b627412a",
+      "ip": "TARAXA_NODE_BOOT_NODE_ADDRESS",
+      "port": 10002
+    }
+  ],
+  "network_id": "testnet",
+  "rpc_port": 7777,
+  "ws_port": 8777,
+  "test_params": {
+    "block_proposer": [
+      0,
+      1,
+      1000,
+      1500
+    ],
+    "pbft": [
+      1000,
+      20,
+      10000,
+      100,
+      1
+    ]
+  },
+  "genesis_state": {
+    "account_start_nonce": 0,
+    "block": {
+      "level": 0,
+      "tips": [],
+      "trxs": [],
+      "sig": "b7e22d46c1ba94d5e8347b01d137b5c428fcbbeaf0a77fb024cbbf1517656ff00d04f7f25be608c321b0d7483c402c294ff46c49b265305d046a52236c0a363701",
+      "hash": "c9524784c4bf29e6facdd94ef7d214b9f512cdfd0f68184432dab85d053cbc69",
+      "sender": "de2b1203d72d3549ee2f733b00b2789414c7cea5",
+      "pivot": "0000000000000000000000000000000000000000000000000000000000000000",
+      "timestamp": 1564617600
+    },
+    "accounts": {
+      "de2b1203d72d3549ee2f733b00b2789414c7cea5": {
+        "balance": 9007199254740991
+      }
+    }
+  }
+}
+EOF
+)
+
+# Install docker
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install -y \
+	apt-transport-https \
+	ca-certificates \
+	curl \
+	gnupg-agent \
+	software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	$(lsb_release -cs) \
+	stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io git
+
+# Gnerate account
+git clone https://github.com/vkobel/ethereum-generate-wallet.git $HOME/ethereum-generate-wallet/
+cd $HOME/ethereum-generate-wallet
+./ethereum-wallet-generator.sh > generated-account.txt
+
+TARAXA_NODE_NODE_SECRET=$(grep Private $HOME/ethereum-generate-wallet/generated-account.txt | cut -d':' -f2  | sed 's/ //g')
+
+#Generate config
+sudo mkdir -p ${TARAXA_NODE_PATH}
+echo ${TARAXA_NODE_CONF} | sudo tee ${TARAXA_NODE_CONF_PATH}
+sudo sed -i s/TARAXA_NODE_BOOT_NODE_ADDRESS/${TARAXA_NODE_BOOT_NODE_ADDRESS}/g $TARAXA_NODE_CONF_PATH
+sudo sed -i s/TARAXA_NODE_NODE_SECRET/${TARAXA_NODE_NODE_SECRET}/g $TARAXA_NODE_CONF_PATH
+
+# Run Taraxa-Node
+sudo docker run -d --name taraxa-node \
+	-v ${TARAXA_NODE_DB_PATH}:/taraxadb \
+	-v ${TARAXA_NODE_CONF_PATH}:/config/conf_taraxa.json \
+	-p 10002:10002 \
+	-p 7777:7777 \
+	-p 8777:8777 \
+	-p 10002:10002/udp \
+	${TARAXA_NODE_DOCKER_IMAGE} --log-verbosity "3" --log-channels PBFT_CHAIN PBFT_MGR VOTE_MGR --conf_taraxa /config/conf_taraxa.json
