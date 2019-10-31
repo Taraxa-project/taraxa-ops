362cca64083ca86dbe948e49b6b5ec2b2d42974c
Justin Snapp <justin@flyjsim.com>
2019-10-31 16:33:08 -0700

    node waits until funded before sending coins to self

--- scripts/send_coins_to_self.py
+++ scripts/send_coins_to_self.py
@@ -3,13 +3,14 @@
 import json
 import os
 import requests
+import time
 
 RECV_ADDRESS = os.environ['RECV_ADDRESS']
 COINS_TO_SEND = os.getenv('COINS_TO_SEND', '1000000')
 RPC_PORT = os.getenv('RPC_PORT', '7777')
 NODE = os.getenv('NODE', 'localhost')
 NONCE = os.getenv('NONCE', '0')
-TPS_RATE = os.getenv('TPS_RATE', '1')
+TPS_RATE = os.getenv('TPS_RATE', '100')
 
 def rpc(node, data):
     request = None
@@ -31,6 +32,29 @@ def jsonrpc_cmd_create_test_coin_trx(number, recv, tps_rate, nonce):
                 }
     return request
 
+def jsonrpc_cmd_get_account_balance(account):
+    request = { "jsonrpc": "2.0", \
+        "id":0, \
+        "method": "get_account_balance", \
+        "params": [{ \
+          "address": account}] \
+        }
+    return request
+
+def send_get_account_balance(node_name, address):
+    try:
+        json_reply = rpc (node_name, jsonrpc_cmd_get_account_balance(address))
+        if json_reply == None:
+            return 0
+        json_reply = json_reply.json()
+
+        result = json_reply["result"]
+        balance = result["value"]
+        return int(balance)
+    except Exception as e:
+        print(e)
+        return 0
+
 def send_coins_to_self(number, recv, tps_rate, nonce, node_name):
     try:
         request = jsonrpc_cmd_create_test_coin_trx(number, recv, tps_rate, nonce)
@@ -40,5 +64,9 @@ def send_coins_to_self(number, recv, tps_rate, nonce, node_name):
     except Exception as e:
         print(e)
     return
+
+
+while send_get_account_balance(NODE, RECV_ADDRESS) == 0:
+    time.sleep(5)  
 
 send_coins_to_self(COINS_TO_SEND,RECV_ADDRESS, TPS_RATE, NONCE, NODE)
