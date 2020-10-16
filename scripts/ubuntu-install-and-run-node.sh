0bac7cddfd92994cb18dac44cf36b8d3d49d48b6
Henrique Fernandes <henrique@toptal.com>
2020-10-16 13:08:13 -0300

    Remove Ask for coins and repository clone

--- scripts/ubuntu-install-and-run-node.sh
+++ scripts/ubuntu-install-and-run-node.sh
@@ -5,10 +5,6 @@
 
 TARAXA_NODE_PATH=/opt/taraxa-node
 TARAXA_NODE_DOCKER_IMAGE=taraxa/taraxa-node:latest
-TARAXA_FAUCET_ADDRESS=${TARAXA_NODE_BOOT_NODE_ADDRESS}
-TARAXA_FAUCET_PORT=5000
-TARAXA_LOCAL_RPC_PORT=7777
-TARAXA_LOCAL_NODE_ADDRESS=localhost
 
 # Install docker and tools
 sudo apt-get remove -y docker docker-engine docker.io containerd runc
@@ -30,9 +26,6 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io git jq npm
 # Install taraxa-cli
 npm install -g taraxa-cli
 
-# Get taraxa-ops repo
-git clone https://github.com/Taraxa-project/taraxa-ops.git /opt/taraxa-ops/
-
 #Generate config
 taraxa -n testnet -d $TARAXA_NODE_PATH
 
@@ -49,20 +42,3 @@ sudo docker run -d --name taraxa-node \
 	-p 10002:10002/udp \
     --restart always \
 	${TARAXA_NODE_DOCKER_IMAGE} --conf_taraxa /taraxa/conf/testnet.json
-
-# Ask for coins
-MY_ADDRESS=$(grep Address /opt/ethereum-generate-wallet/generated-account.txt | cut -d':' -f2 | sed 's/     0x//g')
-curl -d '{"address": "'${MY_ADDRESS}'"}' -H "Content-Type: application/json" -X POST http://${TARAXA_FAUCET_ADDRESS}:${TARAXA_FAUCET_PORT}/nodes/new
-
-# Wait if port isn't ready
-nc -z localhost ${TARAXA_LOCAL_RPC_PORT} || sleep 10
-
-# Add Transaction to self
-export RECV_ADDRESS=${MY_ADDRESS}
-export RPC_PORT=${TARAXA_LOCAL_RPC_PORT}
-export NODE=${TARAXA_LOCAL_NODE_ADDRESS}
-#python3 /opt/taraxa-ops/scripts/send_coins_to_self.py
-#nohup python3 /opt/taraxa-ops/scripts/send_coins_to_self.py > send_coins_to_self.log &
-
-# Add scripts folder to PATH for root user
-echo 'export PATH=$PATH:/opt/taraxa-ops/scripts' >> /root/.profile
