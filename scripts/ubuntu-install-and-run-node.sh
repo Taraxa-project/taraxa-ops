a50fe84551b550379f868b8b53b9623522ae869e
D. Midnight <53611782+dmidnight@users.noreply.github.com>
2020-10-17 15:23:47 -0400

    revert comment

--- scripts/ubuntu-install-and-run-node.sh
+++ scripts/ubuntu-install-and-run-node.sh
@@ -7,21 +7,21 @@ TARAXA_NODE_PATH=/opt/taraxa-node
 TARAXA_NODE_DOCKER_IMAGE=taraxa/taraxa:1.4.2-26
 
 # Install docker and tools
-# sudo apt-get remove -y docker docker-engine docker.io containerd runc
-# sudo apt-get update
-# sudo apt-get install -y \
-# 	apt-transport-https \
-# 	ca-certificates \
-# 	curl \
-# 	gnupg-agent \
-# 	software-properties-common
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# sudo add-apt-repository \
-# 	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-# 	$(lsb_release -cs) \
-# 	stable"
-# sudo apt-get update
-# sudo apt-get install -y docker-ce docker-ce-cli containerd.io git jq
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install -y \
+	apt-transport-https \
+	ca-certificates \
+	curl \
+	gnupg-agent \
+	software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	$(lsb_release -cs) \
+	stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io git jq
 
 # Pull Taraxa-Node
 sudo docker pull ${TARAXA_NODE_DOCKER_IMAGE}
